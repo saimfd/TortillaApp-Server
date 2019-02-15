fdb4322fb13cade5902b56a2bfd1c7dd50a591a8
Saim <saimfd00@gmail.com>
2019-02-15 23:59:44 +0500

    Added GET /todos

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express")
+const mysql = require("mysql")
+const bodyParser = require("body-parser")
 const app = express()
 var port = 1000
+app.use(bodyParser.json());
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "tortilla"
+})
 
-
-app.get("/", (req, res) => {
-    res.send("HOLLA AMIGO");
+app.get("/todos", (req, res) => {
+    con.connect(function(err){
+        if(err) throw err;
+        let sql = "SELECT * FROM tortillas";
+        con.query(sql, function(error, result, fields){
+            res.send(result);
+        });
+    });
 });
 
 app.listen(port, function(){
